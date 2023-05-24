f5fd1adfacb385e00c7d22c518ea16c06d09bf00
pleewson <adrianplewait@gmail.com>
2023-05-24 20:33:21 +0200

    externalJar finished

--- externalJar.java
+++ externalJar.java
@@ -3,10 +3,49 @@ public class externalJar {
     public static void main(String[] args) {
 
         String confirmation = "Yes";
+        String[] speech = {
+                "we",
+                "all",
+                "in",
+                "the",
+                "future"
+        };
+
+        String palindrom = "kamil slimak";
+
+
         System.out.println(reverseString(confirmation));
+        System.out.println(toogleChar(confirmation));
+        System.out.println(stringFromArray(speech));
+        System.out.println(checkPalindrome(palindrom));
     }
 
+
+    //Task1
     public static String reverseString (String stringToReverse){
         return StringUtils.reverse(stringToReverse);
+    }
+
+
+
+    //Task2
+    public static String toogleChar (String str){
+        return StringUtils.swapCase(str);
+    }
+
+
+    //Task3
+    public static String stringFromArray(String[] str){
+        return StringUtils.join(str," ");
+    }
+
+
+
+
+    //Task4
+    public static boolean checkPalindrome(String str){
+        String str1 = StringUtils.deleteWhitespace(str);
+
+        return str1.equals(StringUtils.reverse(str1));
     }
 }
