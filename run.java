921ba1c559a84b123baf9bcaac79f4e7f98359f9
pleewson <adrianplewait@gmail.com>
2023-05-24 14:16:46 +0200

    Path and Files finished:)

--- run.java
+++ run.java
@@ -1,8 +1,12 @@
-public class run {
-    public static void main(String[] args){
+import java.io.IOException;
 
-        Directory1.createDirectory("LordFarquaad");
-        Directory1.createFile("midget.txt");
-        Directory1.copyFile("/Users/adrianplewa/Desktop/trainingFiles/LordFarquaad", "midget.txt", "/Users/adrianplewa/Desktop/trainingFiles/freee/freedom.txt");
+public class run {
+    public static void main(String[] args) throws IOException {
+
+      // Directory1.createDirectory("LordFarquaad");
+       // Directory1.createFile("midget.txt");
+       // Directory1.copyFile("/Users/adrianplewa/Desktop/trainingFiles/LordFarquaad", "midget.txt", "/Users/adrianplewa/Desktop/trainingFiles/freee/freedom.txt");
+       // Directory1.writeToFile("Dunkey.txt");
+        Directory1.readFromFile("quotesShrek");
     }
 }
