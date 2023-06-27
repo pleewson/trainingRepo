15b4b4ae4bd5b6cb9c53635f8a7f4a72fcb888d7
pleewson <adrianplewait@gmail.com>
2023-06-27 20:33:58 +0200

    all methods are written, think how to repair addProduct method

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -6,27 +6,82 @@ public class ShoppingCart {
 
     CartItems newItem;
 
+    public void abc(){
+        for(int i = 0; i < cartItems.length; i++){
+            System.out.println(cartItems[i].getProduct().getName());
+        }
+    }
 
     //zwieksza ilosc
     public void addProduct(Product product, int quantity) {
         increaseArray();
+
+// if (cartItems[i].getProduct().getName().equals(product)) {
+//            cartItems[i].setQuantity(cartItems[i].getQuantity() + quantity);
+//        }
+
+    for (int i = 0; i < cartItems.length; i++) {
+        if(cartItems[i] != null && cartItems[i].getProduct().equals(product)){
+            cartItems[i].setQuantity(cartItems[i].getQuantity() + quantity);
+        }else{
+            //think here
+        }
+}
+
         CartItems newItem = new CartItems(product,quantity);
-
-        if(newItem.getProduct().equals(product)){
-            newItem.setQuantity(newItem.getQuantity()+ quantity);
-        }
-
-        for(int i = 0; i < cartItems.length; i++){
-            cartItems[i] = newItem;
-        }
+        cartItems[cartItems.length-1] = newItem;
 
     }
+
+    //usuwa produkt
+    public void removeProduct(Product product){
+        for(int i = 0; i < cartItems.length; i++){
+            if(cartItems[i].getProduct().equals(product)){
+                cartItems[i] = null;
+            }
+        }
+    }
+
+
+    //aktualizuje produkt
+    public void updateProduct(Product product, int quantity){
+        for(int i = 0; i < cartItems.length; i++){
+            if (cartItems[i].getProduct().equals(product)){
+                cartItems[i].setQuantity(cartItems[i].getQuantity() + quantity);
+            }
+        }
+    }
+
+
+
+    //getter// laczna ilosc produktow
+    public int getTotalQuantity(){
+        int sumQuantity = 0;
+        for(int i = 0; i < cartItems.length; i++){
+            sumQuantity += cartItems[i].getQuantity();
+        }
+        return sumQuantity;
+    }
+
+
+    //getter// laczna wartosc produktow
+    public int getTotalSum(){
+        int totalSum = 0;
+        for(int i = 0; i < cartItems.length; i++){
+          totalSum += cartItems[i].getProduct().getPrice();
+        }
+
+        return totalSum;
+    }
+
+
 
     //drukuje paragon
     int count = 1;
     public void printReceipt (){
         for(int i = 0; i < cartItems.length; i++){
-            System.out.println(count + ": " + cartItems[i].getProduct().getName() + " ID:" + cartItems[i].getProduct().getId() + cartItems[i].getQuantity() + cartItems[i].getProduct().getPrice());
+            System.out.println(count + ": " + cartItems[i].getProduct().getName() + " ID:" + cartItems[i].getProduct().getId() + "  "
+                    + cartItems[i].getQuantity() + "x" + cartItems[i].getProduct().getPrice() + " = " +  (cartItems[i].getQuantity() * cartItems[i].getProduct().getPrice()));
             count++;
         }
     }
@@ -35,8 +90,9 @@ public class ShoppingCart {
 
     public void increaseArray(){
         cartItems = Arrays.copyOf(cartItems, cartItems.length+1);
-        cartItems[cartItems.length-1] = newItem;
     }
+
+
 }
 
 
